fbf42682e861735a28aafd99aa2b40085e63b13e
Nzzinn <nf2086@gmail.com>
2023-04-28 17:09:50 -0300

    Webpack JS

---
+++ Webpack/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('To aprendendo JS rapaziada.');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ Webpack/src/index.js
@@ -0,0 +1,1 @@
+console.log('To aprendendo JS rapaziada.');
